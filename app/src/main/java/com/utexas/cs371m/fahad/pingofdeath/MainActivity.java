417d09f244ed989957fe42aafbb29c5ea0b82814
Fahad Khokhar <fkhokhar@utexas.edu>
2015-11-07 22:59:48 -0600

    adding more stuff

---
+++ app/src/main/java/com/utexas/cs371m/fahad/pingofdeath/MainActivity.java
@@ -0,0 +1,14 @@
+package com.utexas.cs371m.fahad.pingofdeath;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //blah
+    }
+}
